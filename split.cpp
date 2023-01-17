30fa07c6dfea3ac3686e5daa33d99c8d95c8f98e
Guangda Fei <guangdaf@usc.edu>
2023-01-17 02:24:36 +0000

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -11,13 +11,52 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
 
 /* Add a prototype for a helper function here if you need */
+void push(Node*& list, Node*& in);
+// push_back the first element of in to the list and then delete the it from in
+
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  if (in == NULL){return;}
+
+  if (in->value % 2 == 0){
+    push(evens, in);
+    
+  }
+  else if (in->value % 2 == 1){
+    push(odds, in);
+    
+  }
+  split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
+void push(Node*& list, Node*& in){
+
+  if (list == NULL){
+    list = in;
+    Node* temp_1 = in->next;
+    in->next = NULL;
+    in = temp_1;
+    return;
+  }
+  
+  Node* next_element;
+  next_element = list;
+
+  // find the last element of the list
+  while (next_element->next != NULL){
+    next_element = next_element->next;
+
+  }
+  next_element->next = in;
+  Node* temp_2 = in->next;
+  in->next = NULL;
+  in = temp_2;
+  
+}
